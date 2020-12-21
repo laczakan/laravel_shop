c12060c08b5727111e95419e00b90cd56266c6db
Andrzej Laczak <laczakan@yahoo.co.uk>
2020-12-21 10:42:32 +0000

    adding container in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,52 +1,50 @@
 <header>
     <!-- Fixed navbar -->
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="{{ url('')}}">Shop v8</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{url('category')}}">Categories</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{url('products')}}">Products</a>
-                </li>
-                @auth
+        <div class="container clearfix">
+            <a class="navbar-brand" href="{{ url('')}}">Shop v8</a>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{url('basket')}}">My Basket</a>
+                        <a class="nav-link" href="{{url('category')}}">Categories</a>
                     </li>
-                @endauth
-            </ul>
-            <ul class="navbar-nav mr-sm-4">
-                @guest
-                    <li class="nav-item ml-auto">
-                        <a class="nav-link" href="{{url('auth/login')}} ">Log In</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{url('products')}}">Products</a>
                     </li>
-                    <li class="nav-item ml-auto">
-                        <a class="nav-link" href=" {{url('users/create')}}">Register</a>
-                    </li>
-                @endguest
-            </ul>
-            @auth
-                <ul class="navbar-nav mr-sm-4">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Hello {{Auth::user()->name}}!</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="{{ url('auth/logout')}}">Logout</a>
-                            <a class="dropdown-item" href="{{ url('users/'. Auth::id())}}">My profile</a>
-                            @if (Auth::user()->admin)
-                                <a class="dropdown-item" href="{{ url('users/'. Auth::id(). '/categories') }}">Categories panel</a>
-                                <a class="dropdown-item" href="{{ url('users/'. Auth::id()). '/products' }}">Products panel</a>
-                            @endif
-                        </div>
-                    </li>
+                    @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('basket')}}">My Basket</a>
+                        </li>
+                    @endauth
                 </ul>
-            @endauth
+                <ul class="navbar-nav mr-sm-4">
+                    @guest
+                        <li class="nav-item ml-auto">
+                            <a class="nav-link" href="{{url('auth/login')}} ">Log In</a>
+                        </li>
+                        <li class="nav-item ml-auto">
+                            <a class="nav-link" href=" {{url('users/create')}}">Register</a>
+                        </li>
+                    @endguest
+                </ul>
+                @auth
+                    <ul class="navbar-nav mr-sm-4">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Hello {{Auth::user()->name}}!</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="{{ url('auth/logout')}}">Logout</a>
+                                <a class="dropdown-item" href="{{ url('users/'. Auth::id())}}">My profile</a>
+                                @if (Auth::user()->admin)
+                                    <a class="dropdown-item" href="{{ url('users/'. Auth::id(). '/categories') }}">Categories panel</a>
+                                    <a class="dropdown-item" href="{{ url('users/'. Auth::id()). '/products' }}">Products panel</a>
+                                @endif
+                            </div>
+                        </li>
+                    </ul>
+                @endauth
+            </div>
         </div>
     </nav>
 </header>
